bd378b08f75e2b6b59b4a55dfbbe3c783f7fe135
juhyunlee <ljh01099014032@gmail.com>
2025-03-24 11:14:26 +0900

    read 기능 구현

--- articles/urls.py
+++ articles/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     # create
     path('create/', views.create, name='create'),
     # read
-
+    path('', views.index, name='index'), # articles/ 경로가 들어오면 index 보여줌
+    path('<int:id>/', views.detail, name='detail')
     # update
 
     # delete
--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from .forms import ArticleForm
+from .models import Article
 
 # Create your views here.
 def create(request): # 1. 빈 종이를 주는 과정 # 2. 데이터베이스에 저장하는 과정
@@ -16,3 +17,18 @@ def create(request): # 1. 빈 종이를 주는 과정 # 2. 데이터베이스에
     }
 
     return render(request, 'create.html', context)
+
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'index.html',context)
+
+def detail(request, id):
+    article = Article.objects.get(id = id)
+
+    context = {
+        'article': article,
+    }
+    return render(request, 'detail.html', context)
