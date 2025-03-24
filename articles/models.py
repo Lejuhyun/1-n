0e52a206ced35aa15df96e51e01e597863020069
juhyunlee <ljh01099014032@gmail.com>
2025-03-24 12:11:26 +0900

    comment modeling/migration

--- articles/models.py
+++ articles/models.py
@@ -6,3 +6,9 @@ class Article(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now_add = True)
+
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete= models.CASCADE)  # 데이터베이스 관계 설정(내가 연결하고 싶은 데이터)
+                                                                     # on_delete: 상위 데이터가 삭제됐을 때...cascade: 하위 데이터도 삭제함
+       
